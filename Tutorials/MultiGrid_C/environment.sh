0882b1bddfba2da13d46076eee45c58f0adce0b4
David Appelhans <dappelh@us.ibm.com>
2016-09-13 15:13:56 -0400

    Compiling (baseline numbers)

---
+++ Tutorials/MultiGrid_C/environment.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+
+export OMPI_CXX=/gpfs/ess2fs0/walkup/ibmcmp/vacpp/latest/bin/xlc++_r
+export OMPI_CC=/gpfs/ess2fs0/walkup/ibmcmp/vacpp/latest/bin/xlc_r
+
+export OMPI_FC=/gpfs/ess2fs0/l6l/opt/ibm/xlf/15.1.5/bin/xlf_r
+export OMPI_F90=/gpfs/ess2fs0/l6l/opt/ibm/xlf/15.1.5/bin/xlf95_r
