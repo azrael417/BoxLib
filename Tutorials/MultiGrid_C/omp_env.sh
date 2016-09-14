a6549aac203ae27dc52570ee6f6b34298f976712
David Appelhans <dappelh@fstgbfen1.fscluster.com>
2016-09-14 12:03:06 -0400

    adotx running on gpu

--- Tutorials/MultiGrid_C/omp_env.sh
+++ Tutorials/MultiGrid_C/omp_env.sh
@@ -6,3 +6,9 @@ export OMP_PLACES=cores
 export OMP_THREAD_LIMIT=80                                                                             
 export OMP_PROC_BIND=spread,close   
 
+    export OMP_NESTED=FALSE
+    export OMP_NUM_THREADS=80
+    export OMP_PLACES=cores
+    export OMP_THREAD_LIMIT=80
+    export OMP_PROC_BIND=true
+
